ae7936847ba1bbd7920b8a6a60a76881729e3f00
Chris <chrissy.drx@gmail.com>
2023-10-20 18:21:47 +0200

    Initial Commit

---
+++ app/src/main/java/de/xuuniversity/co3/klobuddy/MainActivity.kt
@@ -0,0 +1,11 @@
+package de.xuuniversity.co3.klobuddy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
