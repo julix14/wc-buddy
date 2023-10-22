4b6af7a678af73ed4dc3b0ca8850ed65cadbcd0b
Juliusbecker <julix.becker@gmail.com>
2023-10-22 13:29:15 +0200

    Change: Add Fragment switching

--- app/src/main/java/de/xuuniversity/co3/klobuddy/MainActivity.kt
+++ app/src/main/java/de/xuuniversity/co3/klobuddy/MainActivity.kt
@@ -1,11 +1,44 @@
 package de.xuuniversity.co3.klobuddy
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import de.xuuniversity.co3.klobuddy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frameLayout, FavoritesFragment())
+            .commit()
+
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.action_menu_map -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frameLayout, MapsFragment())
+                        .commit()
+                    true
+                }
+
+                R.id.action_menu_favorites -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frameLayout, FavoritesFragment())
+                        .commit()
+                    true
+                }
+
+                R.id.action_menu_profile -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.frameLayout, ProfileFragment())
+                        .commit()
+                    true
+                }
+
+                else -> false
+            }
+        }
     }
 }
