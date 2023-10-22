07bd3f0ef4e63c72df065afb0829e558bd407d7b
Chris <chrissy.drx@gmail.com>
2023-10-22 13:00:14 +0200

    Add: Basic Visual of Results

--- app/src/main/java/de/xuuniversity/co3/klobuddy/TestActivity.kt
+++ app/src/main/java/de/xuuniversity/co3/klobuddy/TestActivity.kt
@@ -3,6 +3,7 @@ package de.xuuniversity.co3.klobuddy
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.lifecycle.lifecycleScope
 import androidx.room.Room
 import de.xuuniversity.co3.klobuddy.wc.AppDatabase
@@ -14,6 +15,8 @@ class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
+
+        val resultTextView = findViewById<TextView>(R.id.textView)
 
         lifecycleScope.launch {
             // This code will run on the background thread
@@ -28,6 +31,9 @@ class TestActivity : AppCompatActivity() {
             val allWcs = withContext(Dispatchers.IO) {
                 wcDao.getAll()
             }
+
+            val resultText = allWcs.toString()
+            resultTextView.text = resultText
 
             Log.d("WcDao", allWcs.toString())
         }
