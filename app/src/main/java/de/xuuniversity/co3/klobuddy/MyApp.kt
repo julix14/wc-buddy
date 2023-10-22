6c9aa2630400d6a3340c9a18edfdef87913a04c9
Chris <chrissy.drx@gmail.com>
2023-10-22 20:21:18 +0200

    Add: Add Async Firestore Sync

---
+++ app/src/main/java/de/xuuniversity/co3/klobuddy/MyApp.kt
@@ -0,0 +1,20 @@
+package de.xuuniversity.co3.klobuddy
+
+import android.app.Application
+import de.xuuniversity.co3.klobuddy.wc.WcRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+
+class MyApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        val coroutineScope = CoroutineScope(Dispatchers.IO)
+        coroutineScope.launch {
+            WcRepository.upsertReducedWcEntitiesFromFireStore(this@MyApp)
+        }
+
+    }
+}
--- app/src/main/java/de/xuuniversity/co3/klobuddy/wc/WcRepository.kt
+++ app/src/main/java/de/xuuniversity/co3/klobuddy/wc/WcRepository.kt
@@ -2,7 +2,13 @@ package de.xuuniversity.co3.klobuddy.wc
 
 import android.app.Activity
 import android.content.Context
+import android.util.Log
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 import de.xuuniversity.co3.klobuddy.singletons.RoomDatabaseSingleton
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 object WcRepository {
     suspend fun getAllReducedWcEntities(activity: Activity): List<ReducedWcEntity> {
@@ -11,4 +17,34 @@ object WcRepository {
 
         return wcDao.getAllReduced()
     }
+
+    suspend fun upsertReducedWcEntitiesFromFireStore(context: Context){
+        val db = Firebase.firestore
+
+        val coroutineScope = CoroutineScope(Dispatchers.IO)
+
+        db.collection("WcEntity")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    coroutineScope.launch {
+
+                        val dbInstance = RoomDatabaseSingleton.getDatabase(context)
+                        // ToDO: Add type checks
+                        val wcEntity = WcEntity(
+                            document.id,
+                            document.data["description"].toString(),
+                            document.data["latitude"].toString().toDouble(),
+                            document.data["longitude"].toString().toDouble()
+                        )
+                        val dao = dbInstance.wcDao()
+                        dao.upsertWcEntity(wcEntity)
+                    }
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w("DEBUG", "Error getting documents.", exception)
+            }
+    }
+
 }
