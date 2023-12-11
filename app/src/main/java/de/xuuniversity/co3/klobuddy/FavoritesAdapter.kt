8cd7375fff5f6fb86549f4db63515fc2b4a369c0
Juliusbecker <julix.becker@gmail.com>
2023-12-12 00:12:39 +0100

    Fix: Image Coloring of Favorites

--- app/src/main/java/de/xuuniversity/co3/klobuddy/FavoritesAdapter.kt
+++ app/src/main/java/de/xuuniversity/co3/klobuddy/FavoritesAdapter.kt
@@ -1,12 +1,15 @@
 package de.xuuniversity.co3.klobuddy
 
 import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.graphics.drawable.DrawableCompat
 import androidx.recyclerview.widget.RecyclerView
 import de.xuuniversity.co3.klobuddy.wc.WcEntity
-
 
 
 class FavoritesAdapter(
@@ -15,6 +18,7 @@ class FavoritesAdapter(
 ): RecyclerView.Adapter<FavoritesViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavoritesViewHolder {
         val view : View = LayoutInflater.from(context).inflate(R.layout.favorite_item, parent, false)
+
         return FavoritesViewHolder(view)
     }
 
@@ -24,6 +28,41 @@ class FavoritesAdapter(
 
     override fun onBindViewHolder(holder: FavoritesViewHolder, position: Int) {
         val item = items[position]
+
+        // ...
+
+        // Get the drawableLeftCompat of the TextView
+        val drawableLeft = holder.ratingView.compoundDrawables[0]
+        val imageNavigationButton = holder.ratingNavButton.drawable
+
+        if (drawableLeft != null) {
+            // Create a Drawable wrapper
+            val drawableWrapper = DrawableCompat.wrap(drawableLeft)
+            val imageButtonWrapper = DrawableCompat.wrap(imageNavigationButton)
+
+            // Check if dark mode is enabled
+            if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+                // Create a ColorStateList with the light color
+                val colorStateList = ColorStateList.valueOf(Color.WHITE)
+
+                // Apply the ColorStateList to the Drawable wrapper
+                DrawableCompat.setTintList(drawableWrapper, colorStateList)
+                DrawableCompat.setTintList(imageButtonWrapper, colorStateList)
+
+            } else {
+                // Remove the color filter to show the original color of the Drawable wrapper
+                DrawableCompat.setTintList(drawableWrapper, null)
+                DrawableCompat.setTintList(imageButtonWrapper, null)
+            }
+
+            // Set the Drawable wrapper as the drawableLeftCompat of the TextView
+            holder.ratingView.setCompoundDrawablesWithIntrinsicBounds(
+                drawableWrapper,
+                null,
+                null,
+                null
+            )
+        }
         holder.nameView.text = item.description
         val rounded = String.format("%.1f", item.averageRating).toDouble()
         holder.ratingView.text = rounded.toString()
